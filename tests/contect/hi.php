cdc49feed740643919878c4edbfa7fbb315d2bad
cdobby <80838238+cdobby69@users.noreply.github.com>
2021-08-20 19:47:55 +0100

    Update hi.php

--- tests/contect/hi.php
+++ tests/contect/hi.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
+    $email_to = "cdobson@aysgarthschool.co.uk";
     $email_subject = "New form submissions";
 
     function problem($error)
